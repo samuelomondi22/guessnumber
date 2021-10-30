ea4e46dd4b8e812837105674601ef6a917d13bca
samuelomondi22 <omondisam696@gmail.com>
2021-10-30 14:09:11 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carracing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            // picks a random number between 0 and 100
+            int winNum = r.Next(0, 10);
+            // keep track to help know when the game has ended
+            bool win = false;
+            do
+            {
+                Console.WriteLine("Guess the number between 0 and 100: ");
+                string s = Console.ReadLine();
+                int i = int.Parse(s);
+                // if statement to check if it the right number
+                if(i>winNum)
+                {
+                    Console.WriteLine("Too high guess low.");
+                }
+                else if(i<winNum)
+                {
+                    Console.WriteLine("Too low");
+                }
+                else if(i==winNum)
+                {
+                    Console.WriteLine("You win");
+                    win = true;
+                }
+            } while (win==false);
+        }
+    }
+}
---
+++ ReadME.md
@@ -0,0 +1,23 @@
+# Overview
+
+My project is a number guessing game. I decided to go with a simple project because this is my first time doing c#. The point was to help me understand how c# works better. 
+
+
+[Number Guessing Game](https://youtu.be/OckPM-ArAyM)
+
+# Development Environment
+
+* .NET
+* C# Documentation
+
+# Useful Websites
+
+{Make a list of websites that you found helpful in this project}
+* [Decide on a project](https://dev.to/nerdjfpb/15-c-project-ideas-beginner-to-expert-with-tutorial-iio)
+* [.NET](https://dotnet.microsoft.com/)
+* [C# doc](https://docs.microsoft.com/en-us/dotnet/csharp/)
+
+
+# Future Work
+
+Build a more advanced project
